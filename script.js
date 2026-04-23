839f3774305bd5e9dd1cd4d7287974ff80155642
AmenuScott <135128958+AmenuScott@users.noreply.github.com>
2026-04-23 14:57:35 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggle(element) {
+  element.classList.toggle("active");
+}
+
+function openModal() {
+  document.getElementById("modal").style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
